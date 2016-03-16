46913563e94e08b6ac768e111c9cf81ead775dc7
Nathan Tornquist <Tornquist@live.com>
2016-03-16 11:25:48 -0500

    Initial Commit

---
+++ NTImageProcessing/NTImageProcessing.h
@@ -0,0 +1,19 @@
+//
+//  NTImageProcessing.h
+//  NTImageProcessing
+//
+//  Created by Nathan Tornquist on 3/16/16.
+//  Copyright © 2016 Nathan Tornquist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NTImageProcessing.
+FOUNDATION_EXPORT double NTImageProcessingVersionNumber;
+
+//! Project version string for NTImageProcessing.
+FOUNDATION_EXPORT const unsigned char NTImageProcessingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NTImageProcessing/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# NTImageProcessing
+
+NTImageProcessing is a framework that and adds
+support for encryption to basic UIImages.
+
+# License
+
+This project is completely open source and under the MIT license. For full details please see [license.md](LICENSE.md)
